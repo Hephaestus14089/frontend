c71030f9c5c433ef9200b3cb9da13b7333943840
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-10 18:12:53 +0530

    create context
    create the context api provider

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import Category from './components/Category/Category';
 import SingleProduct from './components/SingleProduct/SingleProduct';
 import Newsletter from './components/Footer/Newsletter/Newsletter';
 
-// import AppContext from './utils/context';
+import AppContext from './utils/context';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category/:id" element={<Category />} />
-        <Route path="/product/:id" element={<SingleProduct />} />
-      </Routes>
-      <Newsletter />
-      <Footer />
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </AppContext>
     </BrowserRouter>
   );
 }
