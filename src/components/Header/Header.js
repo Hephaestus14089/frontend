78af8e7b083c3f7404c62122631abcd877c0bec3
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-10 19:11:04 +0530

    create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,26 @@
+import { useEffect, useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// import { TbSearch } from 'react-icons/tb';
+// import { CgShoppingCart } from 'react-icons/cg';
+// import { AiOutlineHeart } from 'react-icons/ai';
+
+import Search from './Search/Search';
+import Cart from '../Cart/Cart';
+
+import { Context } from '../../utils/context';
+
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <Header className="main-header">
+      <div className="header-content">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+      </div>
+    </Header>
   );
 };
 
