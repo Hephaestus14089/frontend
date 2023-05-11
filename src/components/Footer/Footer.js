7d37bead779720e2d25f7977c75a8b6562e9ce97
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-11 16:31:37 +0530

    create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,72 @@
+import {
+  FaLocationArrow,
+  FaMobileAlt,
+  FaEnvelope
+} from 'react-icons/fa';
+
+import Newsletter from './Newsletter/Newsletter';
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="text">
+            Random text...
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-item">
+            <FaLocationArrow />
+            <div className="text">
+              Random text...
+            </div>
+          </div>
+          <div className="c-item">
+            <FaMobileAlt />
+            <div className="text">
+              Random text...
+            </div>
+          </div>
+          <div className="c-item">
+            <FaEnvelope />
+            <div className="text">
+              Random text...
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="title">Categories</div>
+
+          <span className="text">Headphones</span>
+          <span className="text">Speakers</span>
+          <span className="text">Smart Watches</span>
+          <span className="text">Projectors</span>
+        </div>
+
+        <div className="col">
+          <div className="title">Pages</div>
+
+          <span className="text">Home</span>
+          <span className="text">About</span>
+          <span className="text">Privacy Policy</span>
+          <span className="text">Terms and Conditions</span>
+          <span className="text">Returns</span>
+          <span className="text">Contact Us</span>
+        </div>
+      </div>
+
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="text">
+            SwiftBuy (College Project)
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
