b120d47b5d48eb58a6d5dbce3c52ea74840ce1ca
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-11 15:25:16 +0530

    add Banner to Home

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,7 +1,24 @@
+// import BannerImg from "../../../assets/banner-img.png";
+
 const Banner = () => {
   return (
     <div className="Banner">
-      Banner
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Just some random bullshit...
+            which all the sales ads,
+            in a way, actually is.
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+
+        {/* <img src={BannerImg} alt="banner-im" /> */}
+      </div>
     </div>
   );
 };
