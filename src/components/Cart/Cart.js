1733d023ebfc488e1ef97606a4e5ee86bb814674
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-12 20:38:00 +0530

    create Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import { MdClose } from 'react-icons/md';
+import { BsCart } from 'react-icons/bs';
+
+const Cart = () => {
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content"></div>
+    </div>
+  );
+};
+
+export default Cart;
