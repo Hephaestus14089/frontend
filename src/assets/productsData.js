9829d2a501e4f91c80ee0e00bf741cd25c0b15a3
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-19 00:41:03 +0530

    restructure products data
    move dummy data regarding products to single file under assets

---
+++ src/assets/productsData.js
@@ -0,0 +1,72 @@
+export const productsData = [
+  {
+    id: 1,
+    attributes: {
+      title: "JBL Infinity",
+      price: 3500
+    }
+  },
+  {
+    id: 2,
+    attributes: {
+      title: "JBL Clip 3",
+      price: 4000
+    }
+  },
+  {
+    id: 3,
+    attributes: {
+      title: "Raspberry pi",
+      price: 14000
+    }
+  },
+  {
+    id: 4,
+    attributes: {
+      title: "Arduino",
+      price: 11000
+    }
+  },
+  {
+    id: 5,
+    attributes: {
+      title: "AMD Threadripper",
+      price: 1320000
+    }
+  },
+  {
+    id: 6,
+    attributes: {
+      title: "JBL Infinity",
+      price: 3500
+    }
+  },
+  {
+    id: 7,
+    attributes: {
+      title: "AMD Threadripper",
+      price: 1320000
+    }
+  },
+  {
+    id: 8,
+    attributes: {
+      title: "JBL Clip 3",
+      price: 4000
+    }
+  },
+  {
+    id: 9,
+    attributes: {
+      title: "AMD Threadripper",
+      price: 1320000
+    }
+  },
+  {
+    id: 10,
+    attributes: {
+      title: "Raspberry pi",
+      price: 14000
+    }
+  },
+];
